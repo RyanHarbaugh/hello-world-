a69d427345c13bb1856f89c73d9f9f1032a8bdf3
RyanHarbaugh <75967338+RyanHarbaugh@users.noreply.github.com>
2020-12-20 07:34:27 -0800

    new list first n last element, from function

---
+++ listends.py
@@ -0,0 +1,13 @@
+a = [5, 10, 15, 20, 25]
+
+#makes a new list of only the first and last elements of the given list.
+#inside function
+
+#list slicing
+#adding function
+def newListFirstLast(a):
+    firstlast = a [::len(a)-1]
+    print(firstlast)
+
+a = [5, 10, 15, 20, 25]
+newListFirstLast(a)
