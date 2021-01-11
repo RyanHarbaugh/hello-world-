423f0662e00a70eaa4f4849ea8ad1c765a5277c4
RyanHarbaugh <75967338+RyanHarbaugh@users.noreply.github.com>
2021-01-11 07:49:32 -0800

    tic tac toe, winner check

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+#excercise24
+#Ask the user what size game board they want to draw, 
+# and draw it for them to the screen using Python’s print statement.
+
+
+def print_board():
+    size = int(input("Enter the size of the game board"))
+    x = 1
+    while x <= size:
+            print(" ---  "*size + "\n" + "|   | "*size)
+            x += 1
+    print(" ---  "*size)
+    
+if __name__ == "__main__":
+  print_board()
+  input()
+
